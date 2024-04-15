0daa4c37709740667af5c1529ccc97bec66c8272
João Pinto <psp.joao@hotmail.com>
2024-04-15 17:53:07 +0100

    Em desenvolvimento

---
+++ Dockerfile
@@ -0,0 +1,38 @@
+FROM alpine
+
+LABEL maintainer="João Pinto [suport@joaopinto.pt]"
+
+# Install PHP 8.3 and PHP-FPM 8.3
+RUN apk update && apk add --no-cache \
+    php83 \
+    php83-fpm \
+    php83-openssl \
+    php83-phar \
+    php83-iconv \
+    php83-mbstring \
+    nodejs \
+    npm \
+    yarn \
+    nano \
+    github-cli && \
+    rm -rf /var/cache/apk/*
+
+# Install Composer
+RUN php83 -r "copy('https://getcomposer.org/installer', 'composer-setup.php');" && \
+    php83 composer-setup.php && \
+    php83 -r "unlink('composer-setup.php');" && \
+    mv composer.phar /usr/bin/composer
+
+# Move PHP and PHP-FPM binaries to the default path
+RUN mv /usr/bin/php83 /usr/bin/php && \
+    mv /usr/sbin/php-fpm83 /usr/sbin/php-fpm
+
+# Copy custom startup script
+COPY entrypoint/entrypoint.sh /usr/local/bin/entrypoint.sh
+
+# Make the startup script executable
+RUN chmod +x /usr/local/bin/entrypoint.sh
+
+WORKDIR /var/www/html
+
+CMD ["entrypoint.sh"]
---
+++ entrypoint/entrypoint.sh
@@ -0,0 +1,42 @@
+#!/bin/ash
+
+# Define default values for environment variables if they are not set
+PHP_EXTENSIONS=${PHP_EXTENSIONS:-}
+
+# Functions
+
+# Function to check and install PHP extensions
+check_and_install_extension() {
+  installed_extensions=$(php -m)
+  extensions_to_install=""
+
+  for extension in ${PHP_EXTENSIONS}; do
+    # Check if there is any uninstalled extension
+    if ! echo "${installed_extensions}" | grep -wq "${extension}"; then
+      extensions_to_install="${extensions_to_install} ${extension}"
+    fi
+  done
+
+  # Install uninstalled PHP extensions
+  if [ -n "${extensions_to_install}" ]; then
+    echo "Installing PHP extensions: ${extensions_to_install}..."
+    for extension in ${extensions_to_install}; do
+      apk add -q --no-cache php83-"${extension}" > /dev/null 2>&1
+      if ! php -m | grep -wq "${extension}"; then
+        echo "Failed to install PHP extension: ${extension}."
+        exit 1
+      fi
+    done
+    rm -rf /var/cache/apk/*
+    echo "PHP extensions installed successfully."
+  fi
+}
+
+# Main
+
+# Install the required PHP extensions
+if [ -n "${PHP_EXTENSIONS}" ]; then
+  check_and_install_extension
+fi
+
+exec php-fpm -F
